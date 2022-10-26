f30d13c87203f7fe6da0f115b12a763af1e447cc
PedroVe1oso <up201906293@up.pt>
2022-10-26 13:21:48 +0100

    Added missing macros for byte stuffing

--- 1st Lab Work/Project 1 - Protocolo de Ligação de Dados/include/utils.h
+++ 1st Lab Work/Project 1 - Protocolo de Ligação de Dados/include/utils.h
@@ -13,6 +13,8 @@
 #define FLAG 0x7E // 01111110
 // ESC é usado para substituir um octeto idêntico à FLAG que exista no interior da trama
 #define ESC 0x7D // 01111101
+#define ESC_FLAG 0x5E
+#define ESC_ESC 0x5D
 
 // A (Campo de Endereço)
 #define A_T 0x03 // 00000011 Comandos enviados pelo Transmissor e Respostas enviadas pelo Receptor (T->R) -> (R->T)
