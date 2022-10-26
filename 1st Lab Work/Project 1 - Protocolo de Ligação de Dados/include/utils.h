19254a0efa6432f8f4f812871ab80d938e2953ad
PedroVe1oso <up201906293@up.pt>
2022-10-26 12:15:07 +0100

    Added FLAG byte variable

--- 1st Lab Work/Project 1 - Protocolo de Ligação de Dados/include/utils.h
+++ 1st Lab Work/Project 1 - Protocolo de Ligação de Dados/include/utils.h
@@ -7,5 +7,13 @@
 #include "../include/link_layer.h"
 
 
+// Delimitação e cabeçalho
+
+// Todas as tramas são delimitadas por flags ( 01111110 )
+#define FLAG 0x7E // 01111110
+
+// A (Campo de Endereço)
+#define A_T 0x03 // 00000011 Comandos enviados pelo Transmissor e Respostas enviadas pelo Receptor (T->R) -> (R->T)
+#define A_R 0x01 // 00000001 Comandos enviados pelo Receptor e Respostas enviadas pelo Transmissor (R->T) -> (T->R)
 
 #endif // _UTILS_H_
